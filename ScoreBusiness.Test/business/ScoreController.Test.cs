e795dc126bcad1ffa1776eea5962e6a58aa2f543
cah-andrewwaldron <awaldro@gmail.com>
2018-09-15 16:52:01 -0400

    Ready to rumble

--- ScoreBusiness.Test/business/ScoreController.Test.cs
+++ ScoreBusiness.Test/business/ScoreController.Test.cs
@@ -1,7 +1,23 @@
+using NUnit.Framework;
+using ScoreBusiness.domain;
+
 namespace ScoreBusiness.Test.business
 {
+    [TestFixture]
     public class ScoreController_Test
     {
+        private ScoreController subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            subject = new ScoreController();
+        }
         
+        [Test]
+        public void DoesSomething()
+        {
+            Assert.True(true);
+        }
     }
 }
